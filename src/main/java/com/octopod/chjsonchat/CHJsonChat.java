617fb4eb8c39d7a1e8138ccdffd66f3f0ec3ad1c
Octopod <octopodsquad@gmail.com>
2014-02-21 00:54:06 -1000

    updated lifecycle class

--- src/main/java/com/octopod/chjsonchat/CHJsonChat.java
+++ src/main/java/com/octopod/chjsonchat/CHJsonChat.java
@@ -1,5 +1,6 @@
 package com.octopod.chjsonchat;
 
+import com.laytonsmith.PureUtilities.SimpleVersion;
 import com.laytonsmith.PureUtilities.Version;
 import com.laytonsmith.core.extensions.AbstractExtension;
 import com.laytonsmith.core.extensions.MSExtension;
@@ -9,51 +10,7 @@ public class CHJsonChat extends AbstractExtension{
 
 	@Override
 	public Version getVersion() {
-		return new Version() {
-
-			@Override
-			public int getMajor() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			@Override
-			public int getMinor() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			@Override
-			public int getSupplemental() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			@Override
-			public boolean lt(Version other) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public boolean lte(Version other) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public boolean gt(Version other) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public boolean gte(Version other) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-		};
+		return new SimpleVersion(0, 0, 1);
 	}
 
 }
